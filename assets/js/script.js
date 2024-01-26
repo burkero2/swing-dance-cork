f65712d1144c0f8b1e989cfdf32df3bce98b4c6b
burkero2 <>
2024-01-26 16:57:25 +0000

    add functionality to close drop down menu when user clicks anywhere else on the screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
-let navBarBtn = document.getElementById("nav-toggle");
+
+// Declare variables to be used
+let navBarBtn = document.querySelector("#nav-toggle i");
+let navElem = document.getElementsByTagName("nav")[0];
+
+// Add Event Listener on the Drop down button to call the function to toggle the hide feature
 navBarBtn.addEventListener("click", navBarToggle);
 
 function navBarToggle(e) {
-    let navElem = document.getElementsByTagName("nav")[0];
     navElem.classList.toggle("hide");
 }
+
+
+// Anonymous function to hide the menu again if the user clicks anywhere on the screen that's not the button
+document.addEventListener("click", function (e) {
+    console.log(navBarBtn, e.target);
+    if (e.target != navBarBtn) {
+        navElem.classList.add("hide");
+    }
+});
