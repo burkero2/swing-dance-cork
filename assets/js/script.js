ae264deedceff111c16189fc5cfcdaf0837e03fc
burkero2 <>
2024-02-09 16:51:30 +0000

    Styling for mobile of classes timetable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // JQuery Code to handle nav bar button drop on mobile devices
-
 $(document).on("click", function (e) {
     if (e.target !== $(".fa-solid.fa-bars")[0]) {
         $("nav").slideUp('slow');
