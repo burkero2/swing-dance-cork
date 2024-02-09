3cdda361b70b441edfe078dfadbd6a0b1c6ec334
burkero2 <>
2024-02-09 15:43:33 +0000

    Basic structure for the classes table redesigned as separate div-cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,9 @@
-// $(document).on("click", function (e) {
-//     if (e.target === $(".fa-solid.fa-bars")[0]) {
-//         $("nav").slideToggle('slow');
-//     } else {
-//         $("nav").slideUp('slow');
-//     }
-// });
+// JQuery Code to handle nav bar button drop on mobile devices
 
-
-
-// Declare variables to be used
-let navBarBtn = document.querySelector("#nav-toggle i");
-let navElem = document.getElementsByTagName("nav")[0];
-
-// Add Event Listener on the Drop down button to call the function to toggle the hide feature
-navBarBtn.addEventListener("click", navBarToggle);
-
-function navBarToggle(e) {
-    navElem.classList.toggle("hide");
-}
-
-
-// Anonymous function to hide the menu again if the user clicks anywhere on the screen that's not the button
-document.addEventListener("click", function (e) {
-    if (e.target != navBarBtn) {
-        navElem.classList.add("hide");
+$(document).on("click", function (e) {
+    if (e.target !== $(".fa-solid.fa-bars")[0]) {
+        $("nav").slideUp('slow');
+    } else {
+        $("nav").slideToggle('slow');
     }
 });
-
