5e8ba9134d0d7e14875a274f052611715713163b
burkero2 <ronanburke@codeinstitute.net>
2024-02-16 19:27:32 +0000

    EventListener on signup textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 // JQuery Code to handle nav bar button drop on mobile devices
 $(document).on("click", function (e) {
-    if (e.target !== $(".fa-solid.fa-bars")[0]) {
-        $("nav").slideUp('slow');
-    } else {
-        $("nav").slideToggle('slow');
+    let  isMenuIconVisible = $(".fa-solid.fa-bars").is(":visible");
+    // Perform actions only if the menu icon is visible
+    if (isMenuIconVisible) {
+        if (e.target !== $(".fa-solid.fa-bars")[0]) {
+            $("nav").slideUp('slow');
+        } else {
+            $("nav").slideToggle('slow');
+        }
     }
 });
 
@@ -14,7 +18,7 @@ form.addEventListener("change", function (e) {
     let formData = e.target;
     let personalInfoMessage = document.getElementById("signup-personal-feedback")
     let newsletterMessage = document.getElementById("signup-newsletter-feedback")
-    let commentsMessage = document.getElementById("signup-comments-feedback")
+    
 
     // Feedback for the Personal Information Section
     if(formData.name==="name"){
@@ -42,14 +46,25 @@ form.addEventListener("change", function (e) {
         }
     }
 
+    
+})
+
+
+// Event Listener to show message that we'll be in touch if the user leaves a question.
+form.addEventListener("input", function (e) {
+    let formData = e.target;
+    let commentsMessage = document.getElementById("signup-comments-feedback");
     // Feedback for any further questions
     if(formData.name==="questions"){
-        commentsMessage.classList.toggle("hide");
         if(formData.value.trim().length>0){
-            commentsMessage.innerText = "Thanks! We'll be in touch by email soon to answer your query!"
+            commentsMessage.innerText = "Thanks! We'll be in touch by email soon to answer your query!";
+            commentsMessage.classList.remove("hide");
+        } else{
+            commentsMessage.classList.add("hide");
         }
     }
-})
+});
+
 
 form.addEventListener("submit", function(e){
     e.target.submit();
