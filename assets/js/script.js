8ea952a4712a981350e04e9c670e80025646da3c
burkero2 <ronanburke@codeinstitute.net>
2024-02-17 10:29:15 +0000

    Sign Up form styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
+
 // JQuery Code to handle nav bar button drop on mobile devices
 $(document).on("click", function (e) {
-    let  isMenuIconVisible = $(".fa-solid.fa-bars").is(":visible");
+    let isMenuIconVisible = $(".fa-solid.fa-bars").is(":visible");
     // Perform actions only if the menu icon is visible
     if (isMenuIconVisible) {
         if (e.target !== $(".fa-solid.fa-bars")[0]) {
@@ -11,61 +12,60 @@ $(document).on("click", function (e) {
     }
 });
 
-
+// Sign Up Form Event Handling and form validation
 // Event Handling for the form submission including input validation and feedback
-let form = document.getElementById("signup-form");
-form.addEventListener("change", function (e) {
+let signupForm = document.getElementById("signup-form");
+signupForm.addEventListener("change", function (e) {
     let formData = e.target;
     let personalInfoMessage = document.getElementById("signup-personal-feedback")
     let newsletterMessage = document.getElementById("signup-newsletter-feedback")
-    
+
 
     // Feedback for the Personal Information Section
-    if(formData.name==="name"){
-        personalInfoMessage.classList.toggle("hide");
-        if(!formData.value.trim().includes(" ")){
-            personalInfoMessage.style.color="red";
+    if (formData.name === "name") {
+        if (!formData.value.trim().includes(" ")) {
+            personalInfoMessage.classList.remove("hide");
+            personalInfoMessage.style.color = "red";
             personalInfoMessage.innerText = "* Please leave your full name!"
-        } else{
+        } else {
+            personalInfoMessage.classList.add("hide");
             personalInfoMessage.innerText = "";
         }
-        
+
     }
     // Feedback for the newsletter section of the form
-    if(formData.name==="newsletter"){
-        
-        if(formData.value==="yes"){
+    if (formData.name === "newsletter") {
+        if (formData.value === "yes") {
             newsletterMessage.classList.remove("hide");
             newsletterMessage.innerText = "Thanks! You have signed up to our newsletter!"
-        } else if(formData.value==="already"){
+        } else if (formData.value === "already") {
             newsletterMessage.classList.remove("hide");
             newsletterMessage.innerText = "Great to hear! We'll be in touch!"
-        } else{
+        } else {
             newsletterMessage.classList.add("hide");
-            newsletterMessage.innerText ="";
+            newsletterMessage.innerText = "";
         }
     }
-
-    
 })
 
 
 // Event Listener to show message that we'll be in touch if the user leaves a question.
-form.addEventListener("input", function (e) {
+signupForm.addEventListener("input", function (e) {
     let formData = e.target;
     let commentsMessage = document.getElementById("signup-comments-feedback");
     // Feedback for any further questions
-    if(formData.name==="questions"){
-        if(formData.value.trim().length>0){
+    if (formData.name === "questions") {
+        if (formData.value.trim().length > 0) {
             commentsMessage.innerText = "Thanks! We'll be in touch by email soon to answer your query!";
             commentsMessage.classList.remove("hide");
-        } else{
+        } else {
             commentsMessage.classList.add("hide");
         }
     }
 });
 
-
-form.addEventListener("submit", function(e){
+// Sign up form Submit button eventListener
+signupForm.addEventListener("submit", function (e) {
     e.target.submit();
 })
+
